a43561be4acf7d30222b102344ee7c8b0d64b5f9
Meryl Liu <meryl.liu@gmail.com>
2021-07-21 13:54:06 -0400

    added a lot of stuff

---
+++ MapAid/AboutTheDevsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AboutTheDevsViewController.swift
+//  MapAid
+//
+//  Created by Meryl Liu on 7/21/21.
+//
+
+import UIKit
+
+class AboutTheDevsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
